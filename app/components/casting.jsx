e749b95ac6fbe9968e688245a7c11b8badb52b48
Lasek <mary.lasek@sap.com>
2024-08-07 15:54:58 -0300

    Fixing INFO, FRETEBOX size

--- app/components/casting.jsx
+++ app/components/casting.jsx
@@ -6,21 +6,21 @@ export default function Casting() {
   return (
     <>
       <div className="text-center mb-8 mt-10">
-        <h2 className="text-5xl font-bold text-black">LANÇAMENTOS</h2>
-        <p className="text-4xl text-yellow-300">confira nossos lançamentos</p>
+        <h2 className="text-3xl font-bold text-black">LANÇAMENTOS</h2>
+        <p className="text-2xl text-yellow-500">Confira nossos últimos lançamentos</p>
       </div>
 
-      <div className="flex  justify-center gap-2">
-      <div className="max-w-sm bg-white border border-yellow-300 overflow-hidden rounded-lg shadow-md">
+      <div className="flex justify-center gap-6">
+      <div className="max-w-sm bg-white border border-yellow-300 overflow-hidden">
       <img
         src="/exemplo1.png"
         alt="COLAR EXEMPLO - COL0123"
-        className="w-full h-60 object-cover"
+        className="w-full h-60 object-cover p-4"
       />
 
       <div className="p-4 bg-gray-100">
         <h2 className="text-gray-800 text-xl font-bold mb-2">
-          Colar exemplo - COL0123
+          Colar exemplo - COL 0123
         </h2>
 
         <div className="flex items-center mb-2">
@@ -52,27 +52,27 @@ export default function Casting() {
 
         <div className="flex gap-4">
           <button className="flex items-center justify-center bg-yellow-500 text-xs hover:bg-yellow-600 text-yellow-800 py-2.5 px-4 rounded-lg font-bold">
-            <FontAwesomeIcon icon={faShoppingBag} className="mr-2" />
+            <FontAwesomeIcon icon={faShoppingBag} className="mr-2 text-white" />
             COMPRAR
           </button>
 
-          <button className="flex items-center justify-center border-2 border-yellow-300 text-xs hover:bg-gray-100 text-yellow-300 py-2.5 px-4 rounded-lg font-bold">
+          <button className="flex items-center justify-center border border-yellow-300 text-xs hover:bg-gray-100 text-yellow-300 py-2.5 px-4 rounded-lg font-bold">
             <FontAwesomeIcon icon={faHeart} className="mr-2" />
             FAVORITAR
           </button>
         </div>
       </div>
     </div>
-    <div className="max-w-sm bg-white border border-yellow-300 overflow-hidden rounded-lg shadow-md">
+    <div className="max-w-sm bg-white border border-yellow-300 overflow-hidden">
       <img
         src="/exemplo3.png"
         alt="COLAR EXEMPLO - COL0123"
-        className="w-full h-60 object-cover"
+        className="w-full h-60 object-cover p-4"
       />
 
       <div className="p-4 bg-gray-100">
         <h2 className="text-gray-800 text-xl font-bold mb-2">
-          Colar exemplo - COL0123
+          Anel exemplo - ANL 0123
         </h2>
 
         <div className="flex items-center mb-2">
@@ -90,41 +90,41 @@ export default function Casting() {
         </div>
 
         <p className="text-gray-700 text-xl font-bold mb-2">
-          <strong>R$119,90</strong>
+          <strong>R$199,90</strong>
         </p>
 
         <p className="text-gray-600 mb-2 text-xs">
-          <strong>R$99,90</strong> à vista com desconto no Pix
+          <strong>R$179,90</strong> à vista com desconto no Pix
         </p>
 
         <p className="text-gray-600 mb-4 text-xs">
-          ou em até <strong>10x</strong> de <strong>R$11,99</strong> sem
+          ou em até <strong>10x</strong> de <strong>R$19,99</strong> sem
           juros
         </p>
 
         <div className="flex gap-4">
           <button className="flex items-center justify-center bg-yellow-500 text-xs hover:bg-yellow-600 text-yellow-800 py-2.5 px-4 rounded-lg font-bold">
-            <FontAwesomeIcon icon={faShoppingBag} className="mr-2" />
+            <FontAwesomeIcon icon={faShoppingBag} className="mr-2 text-white" />
             COMPRAR
           </button>
 
-          <button className="flex items-center justify-center border-2 border-yellow-300 text-xs hover:bg-gray-100 text-yellow-300 py-2.5 px-4 rounded-lg font-bold">
+          <button className="flex items-center justify-center border border-yellow-300 text-xs hover:bg-gray-100 text-yellow-300 py-2.5 px-4 rounded-lg font-bold">
             <FontAwesomeIcon icon={faHeart} className="mr-2" />
             FAVORITAR
           </button>
         </div>
       </div>
     </div>
-    <div className="max-w-sm bg-white border border-yellow-300 overflow-hidden rounded-lg shadow-md">
+    <div className="max-w-sm bg-white border border-yellow-300 overflow-hidden">
       <img
         src="/exemplo2.png"
         alt="COLAR EXEMPLO - COL0123"
-        className="w-full h-60 object-cover"
+        className="w-full h-60 object-cover p-4"
       />
 
       <div className="p-4 bg-gray-100">
         <h2 className="text-gray-800 text-xl font-bold mb-2">
-          Colar exemplo - COL0123
+          Colar exemplo - COL 0456
         </h2>
 
         <div className="flex items-center mb-2">
@@ -142,41 +142,41 @@ export default function Casting() {
         </div>
 
         <p className="text-gray-700 text-xl font-bold mb-2">
-          <strong>R$119,90</strong>
+          <strong>R$329,90</strong>
         </p>
 
         <p className="text-gray-600 mb-2 text-xs">
-          <strong>R$99,90</strong> à vista com desconto no Pix
+          <strong>R$299,90</strong> à vista com desconto no Pix
         </p>
 
         <p className="text-gray-600 mb-4 text-xs">
-          ou em até <strong>10x</strong> de <strong>R$11,99</strong> sem
+          ou em até <strong>10x</strong> de <strong>R$32,99</strong> sem
           juros
         </p>
 
         <div className="flex gap-4">
           <button className="flex items-center justify-center bg-yellow-500 text-xs hover:bg-yellow-600 text-yellow-800 py-2.5 px-4 rounded-lg font-bold">
-            <FontAwesomeIcon icon={faShoppingBag} className="mr-2" />
+            <FontAwesomeIcon icon={faShoppingBag} className="mr-2 text-white" />
             COMPRAR
           </button>
 
-          <button className="flex items-center justify-center border-2 border-yellow-300 text-xs hover:bg-gray-100 text-yellow-300 py-2.5 px-4 rounded-lg font-bold">
+          <button className="flex items-center justify-center border border-yellow-300 text-xs hover:bg-gray-100 text-yellow-300 py-2.5 px-4 rounded-lg font-bold">
             <FontAwesomeIcon icon={faHeart} className="mr-2" />
             FAVORITAR
           </button>
         </div>
       </div>
     </div>
-    <div className="max-w-sm bg-white border border-yellow-300 overflow-hidden rounded-lg shadow-md">
+    <div className="max-w-sm bg-white border border-yellow-300 overflow-hidden">
       <img
         src="/exemplo4.png"
         alt="COLAR EXEMPLO - COL0123"
-        className="w-full h-60 object-cover"
+        className="w-full h-60 object-cover p-4"
       />
 
       <div className="p-4 bg-gray-100">
         <h2 className="text-gray-800 text-xl font-bold mb-2">
-          Colar exemplo - COL0123
+          Colar exemplo - ANL 0456
         </h2>
 
         <div className="flex items-center mb-2">
@@ -194,25 +194,25 @@ export default function Casting() {
         </div>
 
         <p className="text-gray-700 text-xl font-bold mb-2">
-          <strong>R$119,90</strong>
+          <strong>R$89,90</strong>
         </p>
 
         <p className="text-gray-600 mb-2 text-xs">
-          <strong>R$99,90</strong> à vista com desconto no Pix
+          <strong>R$79,90</strong> à vista com desconto no Pix
         </p>
 
         <p className="text-gray-600 mb-4 text-xs">
-          ou em até <strong>10x</strong> de <strong>R$11,99</strong> sem
+          ou em até <strong>10x</strong> de <strong>R$8,99</strong> sem
           juros
         </p>
 
         <div className="flex gap-4">
           <button className="flex items-center justify-center bg-yellow-500 text-xs hover:bg-yellow-600 text-yellow-800 py-2.5 px-4 rounded-lg font-bold">
-            <FontAwesomeIcon icon={faShoppingBag} className="mr-2" />
+            <FontAwesomeIcon icon={faShoppingBag} className="mr-2 text-white" />
             COMPRAR
           </button>
 
-          <button className="flex items-center justify-center border-2 border-yellow-300 text-xs hover:bg-gray-100 text-yellow-300 py-2.5 px-4 rounded-lg font-bold">
+          <button className="flex items-center justify-center border border-yellow-300 text-xs hover:bg-gray-100 text-yellow-300 py-2.5 px-4 rounded-lg font-bold">
             <FontAwesomeIcon icon={faHeart} className="mr-2" />
             FAVORITAR
           </button>
--- app/components/info.jsx
+++ app/components/info.jsx
@@ -9,36 +9,36 @@ import {
 export default function Info() {
   return (
     <div className="relative">
-      <img
-        src="/slider 1.png"
-        alt="Imagem de uma mulher usando joias Vallore, como brinco e colar"
-        className="w-full object-cover border-t-4 border-b-4 border-yellow-400 relative"
-      />
-      <div className="flex justify-center bg-yellow-300 text-white p-5 max-w-7xl mx-auto space-x-4">
-        <div className="flex items-center space-x-2">
-          <FontAwesomeIcon icon={faGem}></FontAwesomeIcon>
-          <div>
-            <p className="text-2xl font-bold">Vários lançamentos</p>
-            <p className="text-lg">Grandes novidades</p>
-          </div>
+    <img
+      src="/slider 1.png"
+      alt="Imagem de uma mulher usando joias Vallore, como brinco e colar"
+      className="w-full object-cover border-t-2 border-b-2 border-yellow-400 mb-32" // Ajusta a margem inferior para dar mais espaço
+    />
+    <div className="absolute bottom-[-80px] left-0 right-0 bg-yellow-500 text-white py-4 px-6 flex justify-around space-x-4 max-w-6xl mx-auto">
+      <div className="flex items-center space-x-2">
+        <FontAwesomeIcon icon={faGem} className="text-4xl" />
+        <div>
+          <p className="text-sm font-bold">Vários lançamentos</p>
+          <p className="text-xs">Grandes novidades</p>
         </div>
-        <div className="border-l-2 border-white h-[130px]"></div>{" "}
-        <div className="flex items-center space-x-2">
-          <FontAwesomeIcon icon={faCheckCircle}></FontAwesomeIcon>
-          <div>
-            <p className="text-2xl font-bold">Garantia estendida</p>
-            <p className="text-lg">Em todas as peças</p>
-          </div>
+      </div>
+      <div className="border-l-2 border-white h-[80px]" />
+      <div className="flex items-center space-x-2">
+        <FontAwesomeIcon icon={faCheckCircle} className="text-4xl" />
+        <div>
+          <p className="text-sm font-bold">Garantia estendida</p>
+          <p className="text-xs">Em todas as peças</p>
         </div>
-        <div className="border-l-2 border-white h-[130px]"></div>{" "}
-        <div className="flex items-center space-x-2">
-          <FontAwesomeIcon icon={faTruck}></FontAwesomeIcon>
-          <div>
-            <p className="text-2xl font-bold">Frete grátis</p>
-            <p className="text-lg">Acima de R$1000,00</p>
-          </div>
+      </div>
+      <div className="border-l-2 border-white h-[80px]" />
+      <div className="flex items-center space-x-2">
+        <FontAwesomeIcon icon={faTruck} className="text-4xl" />
+        <div>
+          <p className="text-sm font-bold">Frete grátis</p>
+          <p className="text-xs">Acima de R$1000,00</p>
         </div>
       </div>
     </div>
+  </div>
   );
 }
